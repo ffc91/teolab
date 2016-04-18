a621af11e4413273bdce2c7edeeeef1b7869e649
ffc91 <ffc91@outlook.com>
2016-04-18 17:25:15 +0000

    Segunda Subida

--- entorno_de_trabajo_2/programas/programa4.1.pl
+++ entorno_de_trabajo_2/programas/programa4.1.pl
@@ -2,42 +2,35 @@ $_=join("",<>);
 #s/\n/ /g;
 #s/\r/ /g;
 #s/  / /g;
-/.*[\r\n]$/gm;
-my @mymatches = /((.*);){8}.*/gm;
-
-
+#[\r\n]$
 #my $lala = 0;
 #for (@mymatches){
 #print $lala ; print "$_\n" ;
 #$lala++;
 #}
 
+my $patron1 = "(ALIMENTOS FARINACEOS|LECHE Y DERIVADOS)";
+my $patron2 =  "(?:GALLETA|GALLETITAS|DULCE DE LECHE|DULCE DE LECHE CON AGREGADOS|DULCE DE LECHE CON CREMA|DULCE DE LECHE PARA REPOSTERIA)";
+#------ORiginals-----------------
+    #my $patron = $patron1.'(?:(?:.*?);){14}';#.$patron2.'(;(.*?)){2};Uruguay(;(.*?)){7,7};HABILITADO;(.*?);(.*?);.*?';
+    #my $patron = ".*;{23}";#.$patron1."([;]{2,2})".$patron2."(?:(?:.*?);){3}Uruguay([;]{7,7})HABILITADO;(.*);(.*);.*";
 
-
-my %hash;
-while (<>)
-{
-    chomp;
-    my($key, $value) = split /;/;
-    push @{$hash{$key}} , $value ;
-}
-
-foreach my $key (sort keys %hash)
-{
-     print $key . "," . join(";", @{$hash{$key}} ) . "\n" ;
-}
-
-
-
-
+#------V1-----------------
+    #my $patron = $patron1.'(?:(?:.*?);){14}';#.$patron2.'(;(.*?)){2};Uruguay(;(.*?)){7,7};HABILITADO;(.*?);(.*?);.*?';
+#------V2-----------------
+    #my $patron = $patron1.'(?:.*?);'.$patron2;
+#------V3-----------------
+    #my $patron = $patron1.';(?:.*?);'.$patron2.';(?:(?:.*?);){2}'; #;Uruguay(;(.*?)){7,7};HABILITADO;(.*?);(.*?);.*?';
+#------V4-----------------
+    #my $patron = $patron1.';(?:.*?);'.$patron2.';(?:(?:.*?);){2}URUGUAY;(?:(?:.*?);){7}';#HABILITADO;(?:.*?);(?:.*?);.*?';
+#------V5-----------------
+    my $patron = $patron1.';(?:.*?);'.$patron2.';(?:(?:.*?);){2}URUGUAY;(?:(?:.*?);){7}HABILITADO;(?:.*?);(?:.*?);.*?';
 
 
 
-my $patron1 = "ALIMENTOS FARINACEOS|LECHE Y DERIVADOS";
-my $patron2 =  "GALLETA|GALLETITAS|DULCE DE LECHE|DULCE DE LECHE CON AGREGADOS|DULCE DE LECHE CON CREMA|DULCE DE LECHE PARA REPOSTERIA";
- 
- 
-my $patron = ".*";#.$patron1."([;]{2,2})".$patron2."([;]{3,3})Uruguay([;]{7,7})HABILITADO;(.*);(.*);.*";
+print $patron;print "\n";
+my @mymatches = /$patron/gm;
+print "$_\n" for (@mymatches); 
 
 #my $patron = "*";
 #for (@mymatches){
@@ -45,5 +38,5 @@ my $patron = ".*";#.$patron1."([;]{2,2})".$patron2."([;]{3,3})Uruguay([;]{7,7})H
 #    };
 #print "$_\n" for (@mymatches); 
 #my @mymatches2 = ( @mymatches =~ /patron/); 
-print $patron;print "\n";
-print "$\n" for (@mymatches); 
+#print $patron;print "\n";
+#print "$_\n" for (@mymatches2); 
